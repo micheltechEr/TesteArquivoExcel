9d583de30a6e9800c6b44229a5e414478e7871cf
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2020-11-28 01:33:38 -0300

    Update Main.java
    Inclusão do FACADE

--- Main.java
+++ Main.java
@@ -1,36 +1,9 @@
-import java.io.*;
-import javax.swing.*;
-
-public class Main{
-    public static void main(String[] args) throws IOException{
-       JOptionPane.showMessageDialog(null, "Caro usuario,salve o arquivo com extensao .csv");
-        JPanel contentPane =new JPanel();
-        JTextField textField = new JTextField();
-        JFileChooser chooser = new JFileChooser();
-        int selecao = chooser.showOpenDialog(contentPane);
-        if(selecao == JFileChooser.APPROVE_OPTION){
-            File arquivo = chooser.getSelectedFile();
-            textField.setText(arquivo.getAbsolutePath());
-            String ID = JOptionPane.showInputDialog("Digite o ID");
-            String name =JOptionPane.showInputDialog("Digite o nome") ;
-            String age = JOptionPane.showInputDialog("Digite a idade"); 
-            saveRecord(ID,name,age,arquivo);
-        }
-   
+package trabalhoar;
+public class Main {
+    public static void main(String[] args) {
+     Facade f = new Facade(); //Chamada do facade
+     f.exibir();
+           
     }
-    public static void saveRecord(String ID,String name,String age,File filepath){
-        try{
-            FileWriter fw = new FileWriter(filepath,true);
-            BufferedWriter bf = new BufferedWriter(fw);
-            PrintWriter pw = new PrintWriter(bf);
-
-            pw.println(ID+","+name+","+age);
-            pw.flush();
-            pw.close();
-            JOptionPane.showMessageDialog(null,"Record saved");
-        }
-        catch(Exception E){
-            JOptionPane.showMessageDialog(null, "Record save");
-        }
-    }
+    
 }
