c876f88d674c92b8a2ed28a1a45152f293f78805
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2020-11-30 23:17:16 -0300

    Implementação do Singleton

--- Salvar.java
+++ Salvar.java
@@ -1,24 +1,47 @@
 package trabalhoar;
-import java.io.*;
 import javax.swing.*;
+import java.io.*;
+public class Tela {
 
-public class Salvar {
-     public Salvar(){ //Construtor
-        
+    private static  Tela instancia;
+    
+       private Tela(){ }
+       
+    public static  synchronized Tela getInstance(){ //metodo p/ instanciar objeto,retornando ele mesmo
+        if(instancia == null){
+            
+       instancia = new Tela();
     }
-    public void salvar(String ID,String name,String age,File filepath){ //Metodo de salvar
-        try{
-            FileWriter fw = new FileWriter(filepath,true); //Abre o arquivo
-            BufferedWriter bf = new BufferedWriter(fw); //Pega a String para escrita
-            PrintWriter pw = new PrintWriter(bf);//Escreve o registro
+        return instancia;
+    }
+  
+ public void escreverArquivo(){
+Salvar s = Salvar.getInstance(); //Instancia do objeto Salvar
 
-            pw.println(ID+","+name+","+age); //Escreve os valores do parametro no arquivo
-            pw.flush();
-            pw.close(); //Fecha o PrintWritter
-            JOptionPane.showMessageDialog(null,"Arquivo salvo"); //Exibe na tela 
-        }
-        catch(Exception E){
-            JOptionPane.showMessageDialog(null, "Arquivo não salvo");
-        }
+JPanel contentPane = new JPanel();//Instancia do JPanel
+
+JOptionPane.showMessageDialog(null,"Por favor,salve com a extensão csv"); //Mensagem
+
+JTextField textField = new JTextField(); //Caixa de texto
+
+JFileChooser chooser = new JFileChooser(); //Escolhedor
+
+int selecao = chooser.showOpenDialog(contentPane); //Selecao do escolhedor
+
+if(selecao == JFileChooser.APPROVE_OPTION){ //Verfica caso a escolha foi aprovada
+    
+    File arquivo = chooser.getSelectedFile(); //Pega o arquivo
+    
+    textField.setText(arquivo.getAbsolutePath()); //Escreve no arquivo
+    
+    String ID = JOptionPane.showInputDialog("Digite o ID"); //Leitura dos atributos
+    
+    String name =JOptionPane.showInputDialog("Digite o nome") ;
+    
+   String  age = JOptionPane.showInputDialog("Digite a idade"); 
+    
+    s.salvar(ID,name,age,arquivo); //Chamada do metodo para salvar
 }
+    }
 }
+
