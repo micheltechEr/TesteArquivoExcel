76066143400a548bd2efb70d024b807f04a5af11
Ângelo Miguel <48892449+micheltechEr@users.noreply.github.com>
2020-11-30 23:18:11 -0300

    Implementação do Singleton

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 package trabalhoar;
 public class Main {
     public static void main(String[] args) {
-     Facade f = new Facade(); //Chamada do facade
+     Facade f = Facade.getInstance(); //Chamada do facade
      f.exibir();
-           
+     
     }
     
 }
